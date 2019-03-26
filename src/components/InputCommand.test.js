eb0c45f091b61d29787ed264af1434f87b9448b1
John-Michael To <johnmichael_du_78@hotmail.fr>
2019-03-26 14:31:52 +0800

    Adjusting unit tests for InputCommand

--- src/components/InputCommand.test.js
+++ src/components/InputCommand.test.js
@@ -2,25 +2,44 @@ import React from 'react';
 import InputCommand from './InputCommand';
 
 it('Creating spreadsheet command', () => {
-    let command = new InputCommand();
-    expect(command.isCreatingSpreadsheetCommandValid("C 1 1")).not.toBeNull();
-    expect(command.isCreatingSpreadsheetCommandValid("C 10 10")).not.toBeNull();
-    expect(command.isCreatingSpreadsheetCommandValid("C 100 100")).not.toBeNull();
+    let inputCommand = new InputCommand();
+    const command = "C 10 3";
+    const regex = inputCommand.isCreatingSpreadsheetCommandValid(command);
+    expect(regex).not.toBeNull();
+    expect(regex.groups.action).toEqual("C");
+    expect(regex.groups.width).toEqual("10");
+    expect(regex.groups.height).toEqual("3");
 });
 
 it('Inserting number command', () => {
-    let command = new InputCommand();
-    expect(command.isInsertingNumberCommandValid("N 1 1 10")).not.toBeNull();
-    expect(command.isInsertingNumberCommandValid("N 10 10 10")).not.toBeNull();
+    let inputCommand = new InputCommand();
+    const command = "N 3 5 10";
+    const regex = inputCommand.isInsertingNumberCommandValid(command);
+    expect(regex).not.toBeNull();
+    expect(regex.groups.action).toEqual("N");
+    expect(regex.groups.x).toEqual("3");
+    expect(regex.groups.y).toEqual("5");
+    expect(regex.groups.value).toEqual("10");
 });
 
 it('Sum command', () => {
-    let command = new InputCommand();
-    expect(command.isSumCommandValid("S 1 1 2 2 3 3")).not.toBeNull();
-    expect(command.isSumCommandValid("S 1 1 1 1 2 2")).not.toBeNull();
+    let inputCommand = new InputCommand();
+    const command = "S 1 1 2 2 3 3";
+    const regex = inputCommand.isSumCommandValid(command);
+    expect(regex).not.toBeNull();
+    expect(regex.groups.action).toEqual("S");
+    expect(regex.groups.x1).toEqual("1");
+    expect(regex.groups.y1).toEqual("1");
+    expect(regex.groups.x2).toEqual("2");
+    expect(regex.groups.y2).toEqual("2");
+    expect(regex.groups.x3).toEqual("3");
+    expect(regex.groups.y3).toEqual("3");
 });
 
 it('Quit command', () => {
-    let command = new InputCommand();
-    expect(command.isQuitCommandValid("Q")).not.toBeNull();
+    let inputCommand = new InputCommand();
+    const command = "Q";
+    const regex = inputCommand.isQuitCommandValid(command);
+    expect(regex).not.toBeNull();
+    expect(regex.groups.action).toEqual("Q");
 });
