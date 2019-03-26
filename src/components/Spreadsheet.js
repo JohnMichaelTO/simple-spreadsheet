ac70d4d23c24c7ace89e4dcff2801b352c920ee8
John-Michael To <johnmichael_du_78@hotmail.fr>
2019-03-26 17:01:37 +0800

    Use component receiveProps instead of getRequest

--- src/components/Spreadsheet.js
+++ src/components/Spreadsheet.js
@@ -1,36 +1,52 @@
 import React from 'react';
 
 class Spreadsheet extends React.Component {
-    state = {
-        width: 0,
-        height: 0,
-        spreadsheet: [[],[]]
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            request: this.props.request,
+            width: 0,
+            height: 0,
+            spreadsheet: [[],[]]
+        };
+    }
+    
+    componentWillReceiveProps(nextProps) {
+        let request = nextProps.request;
+        let tempState = {
+            ...this.state
+        };
 
-    getRequest = () => {
-        let request = this.props.request;
-        console.log(request);
-        
-        switch (request.action) {
-            case 'C':
-                console.log('Create action');
-                this.create(request.width, request.height);
-                break;
-            case 'N':
-                console.log('Insert action');
-                break;
-            case 'S':
-                console.log('Sum action');
-                break;
-            case 'Q':
-                console.log('Quit action');
-                break;
-            default:
-                console.log("Action doesn't exist");
+        if(this.state.request !== request) {
+            switch (request.action) {
+                case 'C':
+                    tempState.width = request.width;
+                    tempState.height = request.height;
+                    console.log('Create action');
+                    this.create(request.width, request.height);
+                    break;
+                case 'N':
+                    console.log('Insert action');
+                    break;
+                case 'S':
+                    console.log('Sum action');
+                    break;
+                case 'Q':
+                    console.log('Quit action');
+                    break;
+                default:
+                    console.log("Action doesn't exist");
+            }
+
+            this.setState({
+                ...tempState,
+                request: nextProps.request
+            });
         }
-    };
+    }
 
     create = (width, height) => {
+        console.log("width: " + width + ", height: " + height);
         /*
         this.setState({
             width,
@@ -44,11 +60,10 @@ class Spreadsheet extends React.Component {
     };
 
     show = () => {
-        return "width:" + this.state.width + ", height:" + this.state.height + ", spreadsheet:" + this.state.spreadsheet + ", " + JSON.stringify((this.props.request), null, 2);
+        return JSON.stringify((this.state), null, 2);
     };
 
     render() {
-        this.getRequest();
         return (
             this.show()
         );
