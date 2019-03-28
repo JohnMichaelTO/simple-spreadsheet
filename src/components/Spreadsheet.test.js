b4abfa5856cbd7628a940bd71fdd2a66134cc021
John-Michael To <johnmichael_du_78@hotmail.fr>
2019-03-28 18:09:34 +0800

    Adding more unit tests for insert and sum

--- src/components/Spreadsheet.test.js
+++ src/components/Spreadsheet.test.js
@@ -56,7 +56,7 @@ it('isCoordinatesInBoundaries()', () => {
     expect(spreadsheet.isCoordinatesInBoundaries(-2, -3)).not.toBeTruthy();
 });
 
-it('Inserting number', () => {
+it('Inserting valid number', () => {
     let spreadsheet = new Spreadsheet();
     const creationResult = spreadsheet.create(10, 10);
     expect(creationResult).toBeTruthy();
@@ -66,7 +66,21 @@ it('Inserting number', () => {
     expect(spreadsheet.spreadsheet[2][3]).toEqual(5);
 });
 
-it('Sum', () => {
+it('Inserting invalid number', () => {
+    let spreadsheet = new Spreadsheet();
+    const creationResult = spreadsheet.create(10, 10);
+    expect(creationResult).toBeTruthy();
+
+    let insertionResult = spreadsheet.insert(3, 2, -8);
+    expect(insertionResult).not.toBeTruthy();
+    expect(spreadsheet.spreadsheet[2][3]).toEqual("");
+
+    insertionResult = spreadsheet.insert(3, 2, 1000);
+    expect(insertionResult).not.toBeTruthy();
+    expect(spreadsheet.spreadsheet[2][3]).toEqual("");
+});
+
+it('Sum over multiple rows and columns with no empty cells in between', () => {
     let spreadsheet = new Spreadsheet();
     const creationResult = spreadsheet.create(5, 5);
     expect(creationResult).toBeTruthy();
@@ -90,6 +104,108 @@ it('Sum', () => {
     const sumResult = spreadsheet.sum(1, 1, 2, 2, 3, 3);
     expect(sumResult).toBeTruthy();
     expect(spreadsheet.spreadsheet[3][3]).toEqual(20);
+});
+
+it('Sum over multiple rows and columns with 1 empty cell in between', () => {
+    let spreadsheet = new Spreadsheet();
+    const creationResult = spreadsheet.create(5, 5);
+    expect(creationResult).toBeTruthy();
+
+    let insertionResult = spreadsheet.insert(1, 1, 3);
+    expect(insertionResult).toBeTruthy();
+    expect(spreadsheet.spreadsheet[1][1]).toEqual(3);
+
+    insertionResult = spreadsheet.insert(1, 2, 4);
+    expect(insertionResult).toBeTruthy();
+    expect(spreadsheet.spreadsheet[2][1]).toEqual(4);
+
+    insertionResult = spreadsheet.insert(2, 1, 7);
+    expect(insertionResult).toBeTruthy();
+    expect(spreadsheet.spreadsheet[1][2]).toEqual(7);
+
+    const sumResult = spreadsheet.sum(1, 1, 2, 2, 3, 3);
+    expect(sumResult).toBeTruthy();
+    expect(spreadsheet.spreadsheet[3][3]).toEqual(14);
+});
+
+it('Sum over rows with no empty cells in between', () => {
+    let spreadsheet = new Spreadsheet();
+    const creationResult = spreadsheet.create(5, 5);
+    expect(creationResult).toBeTruthy();
+
+    let insertionResult = spreadsheet.insert(1, 1, 5);
+    expect(insertionResult).toBeTruthy();
+    expect(spreadsheet.spreadsheet[1][1]).toEqual(5);
+
+    insertionResult = spreadsheet.insert(1, 2, 4);
+    expect(insertionResult).toBeTruthy();
+    expect(spreadsheet.spreadsheet[2][1]).toEqual(4);
+
+    insertionResult = spreadsheet.insert(1, 3, 6);
+    expect(insertionResult).toBeTruthy();
+    expect(spreadsheet.spreadsheet[3][1]).toEqual(6);
+
+    const sumResult = spreadsheet.sum(1, 1, 1, 3, 1, 4);
+    expect(sumResult).toBeTruthy();
+    expect(spreadsheet.spreadsheet[4][1]).toEqual(15);
+});
+
+it('Sum over rows with 1 empty cell in between', () => {
+    let spreadsheet = new Spreadsheet();
+    const creationResult = spreadsheet.create(5, 5);
+    expect(creationResult).toBeTruthy();
+
+    let insertionResult = spreadsheet.insert(1, 1, 5);
+    expect(insertionResult).toBeTruthy();
+    expect(spreadsheet.spreadsheet[1][1]).toEqual(5);
+
+    insertionResult = spreadsheet.insert(1, 3, 6);
+    expect(insertionResult).toBeTruthy();
+    expect(spreadsheet.spreadsheet[3][1]).toEqual(6);
+
+    const sumResult = spreadsheet.sum(1, 1, 1, 3, 1, 4);
+    expect(sumResult).toBeTruthy();
+    expect(spreadsheet.spreadsheet[4][1]).toEqual(11);
+});
+
+it('Sum over columns with no empty cells in between', () => {
+    let spreadsheet = new Spreadsheet();
+    const creationResult = spreadsheet.create(5, 5);
+    expect(creationResult).toBeTruthy();
+
+    let insertionResult = spreadsheet.insert(1, 1, 5);
+    expect(insertionResult).toBeTruthy();
+    expect(spreadsheet.spreadsheet[1][1]).toEqual(5);
+
+    insertionResult = spreadsheet.insert(2, 1, 4);
+    expect(insertionResult).toBeTruthy();
+    expect(spreadsheet.spreadsheet[1][2]).toEqual(4);
+
+    insertionResult = spreadsheet.insert(3, 1, 6);
+    expect(insertionResult).toBeTruthy();
+    expect(spreadsheet.spreadsheet[1][3]).toEqual(6);
+
+    const sumResult = spreadsheet.sum(1, 1, 3, 1, 4, 1);
+    expect(sumResult).toBeTruthy();
+    expect(spreadsheet.spreadsheet[1][4]).toEqual(15);
+});
+
+it('Sum over columns with 1 empty cell in between', () => {
+    let spreadsheet = new Spreadsheet();
+    const creationResult = spreadsheet.create(5, 5);
+    expect(creationResult).toBeTruthy();
+
+    let insertionResult = spreadsheet.insert(1, 1, 5);
+    expect(insertionResult).toBeTruthy();
+    expect(spreadsheet.spreadsheet[1][1]).toEqual(5);
+
+    insertionResult = spreadsheet.insert(3, 1, 6);
+    expect(insertionResult).toBeTruthy();
+    expect(spreadsheet.spreadsheet[1][3]).toEqual(6);
+
+    const sumResult = spreadsheet.sum(1, 1, 3, 1, 4, 1);
+    expect(sumResult).toBeTruthy();
+    expect(spreadsheet.spreadsheet[1][4]).toEqual(11);
 });
 
 it('Quit', () => {
