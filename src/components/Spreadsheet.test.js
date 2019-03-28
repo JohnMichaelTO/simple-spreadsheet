526019b726c77bd816342d8379aa289f0fa2586f
John-Michael To <johnmichael_du_78@hotmail.fr>
2019-03-28 10:49:39 +0800

    Adding unit tests for 2 independent functions

--- src/components/Spreadsheet.test.js
+++ src/components/Spreadsheet.test.js
@@ -9,6 +9,19 @@ it('Creating spreadsheet', () => {
     expect(spreadsheet.height).toEqual(4);
 });
 
+it('isCoordinatesInBoundaries()', () => {
+    let spreadsheet = new Spreadsheet();
+    const creationResult = spreadsheet.create(15, 15);
+    expect(creationResult).toBeTruthy();
+    expect(spreadsheet.isCoordinatesInBoundaries(4, 6)).toBeTruthy();
+    expect(spreadsheet.isCoordinatesInBoundaries(20, 6)).not.toBeTruthy();
+    expect(spreadsheet.isCoordinatesInBoundaries(8, 18)).not.toBeTruthy();
+    expect(spreadsheet.isCoordinatesInBoundaries(20, 19)).not.toBeTruthy();
+    expect(spreadsheet.isCoordinatesInBoundaries(-1, 6)).not.toBeTruthy();
+    expect(spreadsheet.isCoordinatesInBoundaries(10, -5)).not.toBeTruthy();
+    expect(spreadsheet.isCoordinatesInBoundaries(-2, -3)).not.toBeTruthy();
+});
+
 it('Inserting number', () => {
     let spreadsheet = new Spreadsheet();
     const creationResult = spreadsheet.create(10, 10);
@@ -55,4 +68,12 @@ it('Quit', () => {
     expect(spreadsheet.width).toEqual(0);
     expect(spreadsheet.height).toEqual(0);
     expect(spreadsheet.spreadsheet).toBeNull();
+});
+
+it('formatNumberDisplay()', () => {
+    let spreadsheet = new Spreadsheet();
+
+    expect(spreadsheet.formatNumberDisplay(123)).toBe("123");
+    expect(spreadsheet.formatNumberDisplay(12)).toBe(" 12");
+    expect(spreadsheet.formatNumberDisplay(1)).toBe("  1");
 });
