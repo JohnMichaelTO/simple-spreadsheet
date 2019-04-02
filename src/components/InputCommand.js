1c6f0d22a310bb588b3eb0244c3629e2fbeff88b
John-Michael To <johnmichael_du_78@hotmail.fr>
2019-04-02 16:10:21 +0800

    Allow multiple spaces for the commands

--- src/components/InputCommand.js
+++ src/components/InputCommand.js
@@ -57,9 +57,9 @@ class InputCommand extends React.Component {
         });
     };
     
-    isCreatingSpreadsheetCommandValid = command => /^(?<action>C)\s(?<width>[0-9]+)\s(?<height>[0-9]+)$/.exec(command); // Regex matching the command: C w h
-    isInsertingNumberCommandValid = command => /^(?<action>N)\s(?<x>[0-9]+)\s(?<y>[0-9]+)\s(?<value>[0-9]+)$/.exec(command); // Regex matching the command: N x1 y1 v1
-    isSumCommandValid = command => /^(?<action>S)\s(?<x1>[0-9]+)\s(?<y1>[0-9]+)\s(?<x2>[0-9]+)\s(?<y2>[0-9]+)\s(?<x3>[0-9]+)\s(?<y3>[0-9]+)$/.exec(command); // Regex matching the command: S x1 y1 x2 y2 x3 y3
+    isCreatingSpreadsheetCommandValid = command => /^(?<action>C)\s+(?<width>[0-9]+)\s+(?<height>[0-9]+)$/.exec(command); // Regex matching the command: C w h
+    isInsertingNumberCommandValid = command => /^(?<action>N)\s+(?<x>[0-9]+)\s+(?<y>[0-9]+)\s+(?<value>[0-9]+)$/.exec(command); // Regex matching the command: N x1 y1 v1
+    isSumCommandValid = command => /^(?<action>S)\s+(?<x1>[0-9]+)\s+(?<y1>[0-9]+)\s+(?<x2>[0-9]+)\s+(?<y2>[0-9]+)\s+(?<x3>[0-9]+)\s+(?<y3>[0-9]+)$/.exec(command); // Regex matching the command: S x1 y1 x2 y2 x3 y3
     isQuitCommandValid = command => /^(?<action>Q)$/.exec(command); // Regex matching the command: Q
 
     validate = () => {
